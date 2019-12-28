72e68e19ec8c4203a1687da81e0842cac8fb80d8
yangfubing <1928881525@qq.com>
2019-12-28 18:10:28 +0800

    update google

--- docs/js/google.js
+++ docs/js/google.js
@@ -1,7 +1,31 @@
 // burningmyself.gitee.io
 // Global site tag (gtag.js) - Google Analytics
-/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155098979-1"></script> */
+/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155108285-1"></script> */
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'UA-155098979-1');
+  gtag('config', 'UA-155108285-1');
+
+
+//burningmyself.github.io
+/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155108285-2"></script> */
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-155108285-2');
+
+
+//yangfubing.gitee.io
+/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155108285-3"></script> */
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-155108285-3');  
+
+
+//yangfubing.github.io
+/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155108285-4"></script> */
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-155108285-4');  
