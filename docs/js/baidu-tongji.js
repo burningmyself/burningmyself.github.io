080669b68f244269b6b9df55885e306758164bce
yangfubing <1928881525@qq.com>
2019-12-28 17:18:38 +0800

    add baidu-tongji and google

--- docs/js/baidu-tongji.js
+++ docs/js/baidu-tongji.js
@@ -1,9 +1,35 @@
-<script>
+
 var _hmt = _hmt || [];
 (function() {
-  var hm = document.createElement("script");
+  let hm = document.createElement("script");
+  // rosemarys.gitee.io
   hm.src = "https://hm.baidu.com/hm.js?08563f1498cfbdc12f55e3fc8b133b4b";
-  var s = document.getElementsByTagName("script")[0]; 
+  let s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+  // burningmyself.cn/
+  hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ea33cbaa80e12b2127ea3d44494c0870";
+  s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+  // burningmyself.gitee.io
+  hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?10b96d1600d054d9d9519e5512e0af19";
+  s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+  // burningmyself.github.io/
+  hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?14446117c56cf1fdab44c58081833af8";
+  s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+  // yangfubing.gitee.io
+  hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?66401a22d7cba55cbbdd060204e0f29d";
+  s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+  // yangfubing.github.io
+  hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?dd0db5e74ea516ec6bee7d759094aaaa";
+  s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-</script>
+
---
+++ docs/js/google.js
@@ -0,0 +1,7 @@
+// burningmyself.gitee.io
+// Global site tag (gtag.js) - Google Analytics
+/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155098979-1"></script> */
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-155098979-1');
