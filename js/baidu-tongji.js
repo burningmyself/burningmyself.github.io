287de26039a4345728d9fb765a70fd291534257b
yangfubing <1928881525@qq.com>
2019-12-30 10:42:13 +0800

    Deployed abaf26f with MkDocs version: 1.0.4

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -6,7 +6,7 @@ var _hmt = _hmt || [];
   hm.src = "https://hm.baidu.com/hm.js?08563f1498cfbdc12f55e3fc8b133b4b";
   let s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
-  // burningmyself.cn/
+  // burningmyself.cn
   hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?ea33cbaa80e12b2127ea3d44494c0870";
   s = document.getElementsByTagName("script")[0]; 
@@ -16,7 +16,7 @@ var _hmt = _hmt || [];
   hm.src = "https://hm.baidu.com/hm.js?10b96d1600d054d9d9519e5512e0af19";
   s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
-  // burningmyself.github.io/
+  // burningmyself.github.io
   hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?14446117c56cf1fdab44c58081833af8";
   s = document.getElementsByTagName("script")[0]; 
--- js/google.js
+++ js/google.js
@@ -1,7 +1,14 @@
 // burningmyself.gitee.io
 // Global site tag (gtag.js) - Google Analytics
-/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155098979-1"></script> */
+/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155084439-1"></script> */
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'UA-155098979-1');
+  gtag('config', 'UA-155084439-1');
+
+//yangfubing.gitee.io
+/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155132293-1"></script> */
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-155132293-1'); 
